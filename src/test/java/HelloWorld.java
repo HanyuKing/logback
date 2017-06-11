afcfbf5f9252e9608a5fdfdb2f7081c196234379
wanghanyu8 <wanghanyu8@jd.com>
2017-06-11 15:46:13 +0800

    HelloWorld

---
+++ src/test/java/HelloWorld.java
@@ -0,0 +1,13 @@
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * @author wanghanyu
+ * @create 2017-06-11 15:28
+ */
+
+@Slf4j
+public class HelloWorld {
+    public static void main(String[] args) {
+        log.error("Hello World");
+    }
+}
